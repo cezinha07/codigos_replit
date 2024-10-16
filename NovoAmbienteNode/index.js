e2c38ab2475a7b26cc1de7ed1eeffe2eaf1e4754
cezinha07 <130381085+cezinha07@users.noreply.github.com>
2024-10-16 10:30:02 -0300

    NovoAmbienteNode

---
+++ NovoAmbienteNode/index.js
@@ -0,0 +1,17 @@
+const http = require("http");
+console.dir(http);
+
+/*
+const arr = [1,5,7,9,16,34,2]
+
+console.table(arr)
+console.dir(arr)
+console.error("Error!")
+console.warn("Attemption")
+console.info(arr)
+console.time()
+
+console.log(arr)
+
+console.time()
+*/
